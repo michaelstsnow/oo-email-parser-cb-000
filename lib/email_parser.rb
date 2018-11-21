52279e7245b0da9007a3abfce836f3f00b5cc082
michaelstsnow <michael.st.snow@gmail.com>
2018-11-21 20:31:51 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,13 +14,16 @@ class EmailParser
   def parse
     email_array=@@string.split(/\s/)
     @@string=""
+    clean_unique_emails = nil
     unique_emails=email_array.uniq
     unique_emails.each do |email|
       if email.include? ","
+        clean_unique_emails=email.tr(",","")
       else
+        clean_unique_emails << email
       end
     end
-
+    clean_unique_emails
   end
 
 end
